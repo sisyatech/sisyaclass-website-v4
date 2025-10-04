d019a1d9030edc74b0643cb7e1b952eb1b5a0c4f
sachindra672 <sachindra@sisyaclass.com>
2025-10-04 13:49:36 +0530

    fix: container overflow

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,9 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 const Container = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <div className={cn("mx-auto h-screen max-w-7xl", className)}>{children}</div>;
+  return (
+    <div className={cn("mx-auto h-screen max-w-7xl overflow-hidden", className)}>{children}</div>
+  );
 };
 
 export default Container;
