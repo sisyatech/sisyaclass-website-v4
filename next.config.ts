ae69418d0ef6ec4ccf569a20f3dfd481b1d66a79
sisya1234 <info@sisyaclass.com>
2026-03-06 19:31:06 +0530

    chore: remove console logs in production environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   turbopack: {
     root: path.resolve(process.cwd()),
   },
