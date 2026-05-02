c0601cfbd9e9b9c98b35d9bfe406c7a16d4df408
sisyatech <sisyatech@sisyaclass.com>
2026-05-02 20:08:21 +0530

    gtm addition

--- app/(landing_pages)/10thboards/page.tsx
+++ app/(landing_pages)/10thboards/page.tsx
@@ -5,7 +5,6 @@ import Footer from "@/components/Footer";
 import FooterBottom from "@/components/FooterBottom";
 import BoardContent from "@/components/board/BoardContent";
 import Moto from "@/components/moto";
-import Script from "next/script";
 import { getPageSchemas } from "@/lib/schemaApi";
 import SchemaInjector from "@/components/SchemaInjector";
 
@@ -15,30 +14,6 @@ export default async function BoardPage() {
   return (
     <MobileMenuProvider>
       <SchemaInjector schemas={schemas} />
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`(function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-PMD8KHN9');`}
-      </Script>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-PMD8KHN9"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        />
-      </noscript>
       <Container>
         <Navbar />
         <BoardContent />
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins, Roboto} from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/components/UserContext";
+import Script from "next/script";
+import GTMPageTracker from "@/components/GTMPageTracker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,8 +40,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({
+                  'gtm.start': new Date().getTime(),
+                  event: 'gtm.js'
+              });
+              var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src =
+                  'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-PMD8KHN9');`}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} ${poppins.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PMD8KHN9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <UserProvider>
+          <GTMPageTracker />
           {children}
         </UserProvider>
       </body>
